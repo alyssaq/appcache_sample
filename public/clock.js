ef9c9e2458af92d1032cdac34d83777546ebdf3b
Alyssa <alyssaquek@gmail.com>
2014-09-16 05:15:44 +0800

    Added network status

--- public/clock.js
+++ public/clock.js
@@ -2,7 +2,16 @@ function init() {
   document.getElementById('clock').textContent = new Date();
   setTimeout(init, 100);
 
+  networkStatusHandler();
+  window.addEventListener('online', networkStatusHandler, false);
+  window.addEventListener('offline', networkStatusHandler, false);
   checkCache();
+}
+
+function networkStatusHandler(event) {
+  var statusElem  = document.getElementById('networkstatus');
+  var status = navigator.onLine ? 'online' : 'offline';
+  statusElem.className = statusElem.innerHTML = status;
 }
 
 function handler(msg) {
